6a6ef1cca9cceee874538782db9e0f0a94f16208
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2021-11-25 16:10:19 +0100

    Update Kanban-Board
    Updated Style
    Added ability to change order and row

---
+++ Backend/data/Task.js
@@ -0,0 +1,8 @@
+class Task {
+    constructor(text, tag){
+        this.text = text;
+        this.tag = tag;
+        this.state = "todo";
+        this.date = Date;
+    }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,9 @@
 
 import { serve } from "https://deno.land/std@0.74.0/http/server.ts"; 
 
-const s = serve({ port: 1877 }); 
+const s = serve({ port: 8000 }); 
 
-console.log("http://localhost:1877/"); 
+console.log("http://localhost:8000/"); 
 
 for await (const req of s) { 
 
