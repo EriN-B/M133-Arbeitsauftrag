32437f7822be36454711f9956fd53fc0b6fddc49
Erin Bachmann <57983942+EriN-B@users.noreply.github.com>
2021-12-12 21:36:01 +0100

    Add task loader
    Tasks now get loaded into the board

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,7 +1,39 @@
+'use strict'
 
-async function deleteItem(){
+async function initialize() {
+    const tasks = await getAllTasks();
+
+    for (let task of tasks) {
+        let item = 
+        `<li class="mt-2" id="c1">
+            <div class="block p-5 bg-white rounded shadow">
+            <div class="flex justify-between">
+                <p>${task.text}</p>
+                <span class="material-icons text-gray-500">
+                <a onclick="deleteItem()" class="large material-icons icon-red cursor-pointer">delete</a>
+                </span>
+            </div>
+            <div class="mt-5 flex justify-between">
+                <p class="text-sm text-gray-600 ">${task.date}</p>
+                <div>
+                <span class="inline-flex items-center rounded px-2 py-1 ${task.bgstyle}">
+                    <svg class="h-2 w-2 text-indigo-500" viewBow="0 0 8 8" fill="${task.color}">
+                    <circle cx="4" cy="4" r="3" />
+                    </svg>
+                    <span class="ml-2 text-sm font-medium ${task.textstyle}">
+                    ${task.tag}
+                    </span>
+                </span>
+                </div>
+            </div>
+            </div>
+        </li>`;
+        document.getElementById("col1").innerHTML += item;
+    }
+}
+
+async function deleteItem() {
     window.alert("test");
-    
 }
 
 async function getAllTasks() {
@@ -15,7 +47,7 @@ async function addTask() {
     const id = await getUUID();
     await fetch("http://localhost:8000/api/list", {
         method: "POST",
-        body: JSON.stringify({id: id, text: "test create task", tag: "Feature", status: "todo", date: "Dez 12"})
+        body: JSON.stringify({ id: id, text: "test create task", tag: "Feature", status: "todo", date: "Dez 12" })
     });
     console.log(id);
 }
@@ -25,3 +57,5 @@ async function getUUID() {
         method: "GET"
     });
 }
+
+initialize()
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Arbeitsauftrag-m133
 
 ## Run Project
-> deno run --allow-read --allow-net Backend/server.js
+> deno run --allow-read --allow-net server.js
