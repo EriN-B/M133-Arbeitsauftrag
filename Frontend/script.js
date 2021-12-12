57cfad8c0d39fa84e0b0438f8face93dfa4b032c
Moritz Wicki <wickimoritz09@gmail.com>
2021-12-12 19:29:41 +0100

    add part of api connection

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,3 +1,28 @@
-function deleteItem(){
+
+async function deleteItem(){
     window.alert("test");
+    
+}
+
+async function getAllTasks() {
+    const response = await fetch("http://localhost:8000/api/list", {
+        method: "GET"
+    });
+    return await response.json();
+}
+
+async function addTask() {
+    const id = await getId();
+    await fetch("http://localhost:8000/api/list", {
+        method: "POST",
+        body: JSON.stringify({id: id, text: "test create task", tag: "Feature", status: "todo", date: "Dez 12"})
+    });
+    console.log(id);
+}
+
+async function getId() {
+    const response = await fetch("http://localhost:8000/api/id", {
+        method: "GET"
+    });
+    return response;
 }
